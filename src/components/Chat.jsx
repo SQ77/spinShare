5f26754fb69d80b31ba9b5dcc389ab271d843753
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2024-10-03 20:47:18 +0800

    Implement chat window

---
+++ src/components/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chat = () => {
+    return (
+        <div className="w-[90vw] h-[90vh] bg-[rgba(17,25,40,0.75)] backdrop-blur-[19px] backdrop-saturate-[180%] rounded-[12px] p-4 border-2 border-gray-300">
+            Chat
+        </div>
+    );
+};
+
+export default Chat;
