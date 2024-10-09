2e1cf3e02665a0340ccc8ab61ac224f2e4b497d5
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2024-10-09 18:48:54 +0800

    Improve home design

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,15 @@
 import Hero from "../Hero";
 import Features from "../Features";
+import ContactForm from "../ContactForm"; // Import the ContactForm component
 
 const Home = () => {
   return (
     <>
-    <Hero />
-    <Features />
-    <p className="ml-4 mb-4">Help to improve SpinShare by giving{' '}  
-      <a href="https://forms.gle/tSN6GMSWoa5RPDYK7" className="text-blue-600 hover:underline">
-          feedback
-      </a>
-    </p>
+      <Hero />
+      <Features />
+      <div className="bg-indigo-700 py-8"> {/* Added purple background */}
+        <ContactForm /> 
+      </div>
     </>
   );
 };
