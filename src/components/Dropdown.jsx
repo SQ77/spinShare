3c0d84ea076d2e13b66869cbac71f792effb32ed
SQ77 <andrewzsq@gmail.com>
2025-01-05 16:25:38 +0800

    Improve studio location dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaChevronDown } from "react-icons/fa";
 
 const Dropdown = ({ currLocation, handleClick, studio }) => {
     const [isOpen, setIsOpen] = useState(false);
+
     const dropdownRef = useRef(null);
     let locations = [];
     if (studio === "absolute") {
@@ -29,7 +31,7 @@ const Dropdown = ({ currLocation, handleClick, studio }) => {
     }, []);
 
   return (
-    <div className="relative inline-block text-left" ref={dropdownRef}>
+    <div className="relative inline-block" ref={dropdownRef}>
       <div>
         <button
           type="button"
@@ -40,25 +42,17 @@ const Dropdown = ({ currLocation, handleClick, studio }) => {
           onClick={toggleDropdown}
         >
           {currLocation}
-          <svg
-            className="-mr-1 ml-2 h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.23 7.21a.75.75 0 011.06 0L10 10.94l3.71-3.73a.75.75 0 111.06 1.06l-4 4a.75.75 0 01-1.06 0l-4-4a.75.75 0 010-1.06z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <FaChevronDown
+            className={`-mr-1 ml-2 h-4 w-4 transform transition-transform duration-300 ${
+                isOpen ? "rotate-180" : "rotate-0"
+            }`}
+          />
         </button>
       </div>
       
       {isOpen && (
         <div
-          className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+          className="origin-top-left absolute left-0 text-left mt-2 w-56 rounded-md shadow-lg bg-gray-200 ring-1 ring-black ring-opacity-5"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="options-menu"
@@ -66,8 +60,9 @@ const Dropdown = ({ currLocation, handleClick, studio }) => {
             <div className="py-1" role="none">
                 {locations.filter(loc => loc !== currLocation).map((location, index) => (
                     <div role="menuitem" key={index} onClick={() => handleClick()}>
-                    <NavLink to={`/${studio}/${location}`}
-                    className="block px-4 py-2 text-sm text-black hover:bg-gray-200"
+                    <NavLink 
+                      to={`/${studio}/${location}`}
+                      className="block px-4 py-2 text-sm text-black hover:bg-gray-300"
                     >
                         {location}
                     </NavLink> 
